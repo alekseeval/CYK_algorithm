cbda0341e4dc0051e36599858362523a4f1dfc6d
HyperMarshmallow <alekseeval2000@mail.ru>
2019-12-20 21:57:54 +0800

    Some little changes

--- src/com/company/GrammarChecker.java
+++ src/com/company/GrammarChecker.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class GrammarChecker {
 
     String[][] grammar;
@@ -32,8 +29,7 @@ public class GrammarChecker {
             ans_mat[i][i] = r.toString();
         }
 
-//        //Fill the rest of the matrix
-        // Тут все по картинке понятна (НАВЕРНА)
+        //Fill the rest of the matrix
         for (int j = 1; j < ans_mat.length; j++) {
             for (int i = 0; i < ans_mat.length - j; i++) {
                 String value = getValueForPosition(i, i + j, ans_mat);
@@ -61,28 +57,25 @@ public class GrammarChecker {
 
     //Checks if the passed string can be achieved for the grammar
     private String check(String a){
-        StringBuilder to_ret = new StringBuilder();
-        int count;
+        StringBuilder result = new StringBuilder();
         for (String[] strings : grammar) {
-            for (count = 0; count < strings.length; count++) {
-                if (strings[count].equals(a)) {
-                    to_ret.append(strings[0]);
+            for (int i = 1; i < strings.length; i++) {
+                if (strings[i].equals(a)) {
+                    result.append(strings[0]);
                 }
             }
         }
-        return to_ret.toString();
+        return result.toString();
     }
 
     //Makes all possible combinations out of the two string passed
     private String combine(String a, String b){
-        StringBuilder to_ret = new StringBuilder();
-        String temp;
+        StringBuilder result = new StringBuilder();
         for(int i = 0; i < a.length(); i++){
             for(int j = 0; j < b.length(); j++){
-                temp = a.charAt(i) + "" +  b.charAt(j);
-                to_ret.append(check(temp));
+                result.append(check(a.charAt(i) + "" +  b.charAt(j)));
             }
         }
-        return to_ret.toString();
+        return result.toString();
     }
 }
